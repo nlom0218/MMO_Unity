9c491cedb3725ff295b6b0685a0e79cc2440dfb1
nlom0218 <nlom0218@naver.com>
2022-05-15 14:24:31 +0900

    COmponent 패턴

--- Assets/TestUnity.cs
+++ Assets/TestUnity.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloUnity : MonoBehaviour
+public class TestUnity : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(1.0f, 1.0f, 1.0f));
+
     }
+
+
 }
