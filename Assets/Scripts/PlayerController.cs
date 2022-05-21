613c23ef74c4f99f2a85dab55933a90ee859069e
nlom0218 <nlom0218@naver.com>
2022-05-21 11:28:45 +0900

    Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,39 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 1. 위치 백터
-// 2. 방향 백터
-struct MyVector
-{
-    float x;
-    float y;
-    float z;
-
-    public float magnitude
-    {
-        get { return Mathf.Sqrt(x * x + y * y + z * z); }
-    }
-
-    public MyVector normalized
-    {
-        get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); }
-    }
-
-    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
-    public static MyVector operator +(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-    public static MyVector operator -(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-    public static MyVector operator *(MyVector a, float d)
-    {
-        return new MyVector(a.x * d, a.y * d, a.z * d);
-    }
-}
-
 public class PlayerController : MonoBehaviour
 {
 
@@ -42,16 +9,6 @@ public class PlayerController : MonoBehaviour
     float _speed = 10.0f;
     void Start()
     {
-        MyVector pos = new MyVector(0.0f, 10.0f, 0.0f);
-        pos += new MyVector(0.0f, 2.0f, 0.0f);
-
-        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
-        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
-        MyVector dir = to - from;
-
-        dir = dir.normalized;
-
-        MyVector newPos = from + dir * _speed;
 
         // 방향백터
         // // 1. 거리(크기) magnitude
@@ -62,8 +19,21 @@ public class PlayerController : MonoBehaviour
     // GameObject (Player)
     // // Transform
     // // PlayerController (*)
+
+    float _yAngle = 0.0f;
     void Update()
     {
+        _yAngle += Time.deltaTime * 100.0f;
+
+        // 절대 회전값
+        // transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
+
+        // +- detal
+        // transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
+
+        // transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
+
         // transform.TransformDirection(): Local => World 
         // transform.InverseTransformDirection(): World => Local
 
@@ -71,12 +41,28 @@ public class PlayerController : MonoBehaviour
         // transform.position.normalized;
 
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        {
+            // transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        {
+            // transform.rotation = Quaternion.LookRotation(Vector3.back);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.A))
-            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
+        {
+            // transform.rotation = Quaternion.LookRotation(Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.D))
-            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
+        {
+            // transform.rotation = Quaternion.LookRotation(Vector3.right);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
     }
 }
